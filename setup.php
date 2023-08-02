2229868518e80437a0a216f58673d86df983e466
Barbesange, Jason <jason.barbesange-ext@araymond.com>
2023-08-02 14:59:56 +0200

    test push

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_FIELDS_VERSION', '1.20.7');
+define('PLUGIN_FIELDS_VERSION', '1.20.8');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_FIELDS_MIN_GLPI", "10.0.0");
